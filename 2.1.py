6bd5c736bd30e4b07f9630bda5f35d5942fbf0a7
Doan Ngoc Bich <doanbich0910@gmail.com>
2026-03-16 10:22:43 +0700

    2.1.py

---
+++ 2.1.py
@@ -0,0 +1,10 @@
+seconds = 42 * 60 + 42
+print("1. Số giây là:", seconds)
+miles = 10 / 1.61
+print("2. Số dặm là:", miles)
+total_minutes = 42 + 42/60 
+pace_per_mile = total_minutes / miles
+print("3a. Vận tốc trung bình (phút/dặm):", pace_per_mile)
+total_hours = total_minutes / 60
+miles_per_hour = miles / total_hours
+print("3b. Vận tốc trung bình (dặm/giờ):", miles_per_hour)
